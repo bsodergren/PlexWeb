0e2ccfdb481b5b2b039b18a12cd2b76356890071
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-11-18 07:07:32 +0000

    sas

--- src/Controller/GridDisplayController.php
+++ src/Controller/GridDisplayController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\VideoDetails;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class GridDisplayController extends AbstractController
 {
     #[Route('/grid/display', name: 'app_grid_display')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $product = $entityManager->getRepository(VideoDetails::class);
+        dd($product);
+
         return $this->render('grid_display/index.html.twig', [
             'controller_name' => 'GridDisplayController',
         ]);
